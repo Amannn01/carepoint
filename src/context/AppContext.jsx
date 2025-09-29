932d9e2bddb413e1efce161685c9779ce4c82d43
Amannn01 <amanjainsanghvi19@gmail.com>
2025-09-29 22:14:03 +0530

    first commit

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,17 @@
+// import React, { createContext, useState } from "react";
+import React from "react";
+import { createContext } from "react";
+import { doctors } from "../assets/assets";
+
+export const AppContext =createContext()
+    const AppContextProvide =(props)=>{
+        const value={
+            doctors
+        }
+        return (
+            <AppContext.Provider value={value}>
+                {props.children}
+            </AppContext.Provider>
+        )
+    }
+    export default AppContextProvide
